52ef17c6c730ae5f87eca3f2600b44a1a4d827b4
MarlonChi <marlonchiodelli@hotmail.com>
2024-06-15 16:52:09 -0300

    feat: org creation

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,28 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.post("/orgs", async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    author_name: z.string(),
+    email: z.string().email(),
+    whatsapp: z.string(),
+    password: z.string(),
+    cep: z.string(),
+    state: z.string(),
+    city: z.string(),
+    neighborhood: z.string(),
+    street: z.string(),
+    latitude: z.number(),
+    longitude: z.number(),
+  });
 
-prisma.org.create({
-  data: {
-    name: "Amigos",
-    author_name: "Marlon",
-    email: "org.amigos@teste.com",
-    cep: "",
-    city: "",
-    latitude: 123,
-    longitude: 123,
-    neighborhood: "",
-    password: "123",
-    state: "RS",
-    street: "",
-    whatsapp: "999999999",
-  },
+  const body = registerBodySchema.parse(request.body);
+
+  await prisma.org.create({
+    data: body,
+  });
 });
