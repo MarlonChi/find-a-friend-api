3af296ac385b07de962748714b8443a8ff5d235d
MarlonChi <marlonchiodelli@hotmail.com>
2024-06-15 16:37:12 -0300

    chore: docker config

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.org.create({
+  data: {
+    name: "Amigos",
+    author_name: "Marlon",
+    email: "org.amigos@teste.com",
+    cep: "",
+    city: "",
+    latitude: 123,
+    longitude: 123,
+    neighborhood: "",
+    password: "123",
+    state: "RS",
+    street: "",
+    whatsapp: "999999999",
+  },
+});
